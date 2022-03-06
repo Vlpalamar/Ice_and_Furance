720f3710e4ca23493f8819a8bbbaed2fbe98a665
Vlpalamar <65705755+Vlpalamar@users.noreply.github.com>
2022-03-06 13:04:58 +0200

    Start of Work
    common scene

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    // Start is called before the first frame update
+    #region references  
+    private Rigidbody2D rb;
+    #endregion
+    
+    private float horizontalMove;
+    Vector2 targetVelocity= new Vector2();
+   
+    
+    [SerializeField] private float speed;
+
+    void Start()
+    {
+        this.rb= GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove= Input.GetAxis("Horizontal")*speed*10;
+        
+    }
+
+    void FixedUpdate()
+    {
+       
+        targetVelocity.x= horizontalMove*Time.fixedDeltaTime;
+        targetVelocity.y= rb.velocity.y;
+        rb.velocity=targetVelocity;
+        
+    }
+}
